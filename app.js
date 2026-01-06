7953d5ace5a12a14c648a86077f4b7ea26d9fa6e
Zadoque <zadoquepiresdedeus@gmail.com>
2026-01-06 09:18:27 -0300

    added logic for not just fecth questions, but also toggle and start quiz

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 class QuizApp {
     constructor() {
+        this.content_start = document.querySelector(".content-start");
         this.start_quiz_button = document.querySelector("#start-quiz-button");
         this.category = document.querySelector("#quiz-category");
         this.difficulty = document.querySelector("#quiz-difficulty");
         this.limit = document.querySelector("#quiz-limit");
-        this.categories = ["Linux", "DevOps", "Networking", "Programming", "Cloud", "Docker", "Kubernetes"];
+        this.categories = ["Linux", "DevOps", "Networking", "react", "Cloud", "Docker", "Kubernetes"];
         this.difficulties = ["Hard", "Medium", "Easy"];
         this.limits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
             "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
         this.api_key = "AD9sPtCHx3021gY6h39Qkt2ypsr0r1GAm7Y0i9oF";
         this.basic_url = "https://quizapi.io/api/v1/questions?";
+
+        //Questions part:
+        this.content_question = document.querySelector(".content-question");
+        this.subimit_button = document.querySelector("#question-subimit-button");
+        this.next_button = document.querySelector("#question-next-button");
+        this.previous_button = document.querySelector("#question-previous-button");
+        this.progress_bar = document.querySelector("#progress-bar");
+        this.questions = [];
         this.init();
     }
     init() {
@@ -18,11 +27,20 @@ class QuizApp {
 
     bindEvents() {
         this.start_quiz_button.addEventListener("click", () => {
-            this.handleStartQuiz();
+            this.handleStartQuizButton();
+        });
+        this.subimit_button.addEventListener("click", () => {
+            this.handleSubimitQuestionButton();
+        });
+        this.next_button.addEventListener("click", () => {
+            this.handleNextQuestionButton();
+        });
+        this.previous_button.addEventListener("click", () => {
+            this.handlePreviousQuestionButton();
         });
     }
 
-    handleStartQuiz() {
+    handleStartQuizButton() {
         let category = this.category.value;
         let difficulty = this.difficulty.value;
         let limit = this.limit.value;
@@ -50,13 +68,25 @@ class QuizApp {
     async fetchQuestions(category, difficulty, limit) {
         const url = `${this.basic_url}apiKey=${this.api_key}&category=${category}&difficulty=${difficulty}&limit=${limit}`;
         try {
-            const data = await this.getQuizQuestions(url);
-            console.log(data);
+            this.questions = await this.getQuizQuestions(url);
+            this.toogleStartQuestion();
+            
+
 
         } catch (error) {
             console.log(`Error: ${error}`);
         }
 
+    }
+    toogleStartQuestion(){
+        console.log(window.getComputedStyle(this.content_start).display);
+        if(window.getComputedStyle(this.content_start).display == "flex"){
+            this.content_start.style.display = "none";
+            this.content_question.style.display = "flex";
+        } else{
+            this.content_question.style.display = "none";
+             this.content_start.style.display = "display";
+        }
     }
     async getQuizQuestions(url) { 
         try {
@@ -74,9 +104,7 @@ class QuizApp {
     handleInvalidParameters() {
         alert("Please provide the 3: category, difficulty and limit");
     }
-
 }
-
 document.addEventListener("DOMContentLoaded", () => {
     new QuizApp();
 });
